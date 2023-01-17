e7334c9183bb03d0c2ce5eaef89cb87983b71f50
Antonio-D-Lopez <Antonio.D.Lopez@outlook.com>
2023-01-17 02:09:52 +0000

    My first experiment with python object oriented programming.

---
+++ python-oop-test1.py
@@ -0,0 +1,17 @@
+class Guitar:
+
+    def __init__(self, maker, model):
+        self.maker = maker
+        self.model = model
+
+    def play_chord(self,x):
+        print("That" , x, " chord sounds great on this ", self.maker, " ", self.model)
+    def what_guitar(self):
+        print("This guitar is a ", self.maker, self.model, "!")
+
+
+guitar1 = Guitar("fender", "stratocaster")
+guitar2 = Guitar("gibson", "SG")
+
+guitar1.play_chord("A")
+guitar2.what_guitar()

fc45d58b1cafb542491c7f8e21dab76688ce001b
Antonio-D-Lopez <Antonio.D.Lopez@outlook.com>
2023-03-04 02:36:31 +0000

    renamed to avoid conflicts with test_ naming standard

---
+++ Circle_object_test.py
@@ -0,0 +1,14 @@
+from circleModule import Circle
+
+## Let's create our first instantiation of the circle class, with object circle1
+circle1 = Circle(2) # we will supply the argument 2 to define the radius parameter
+## lets test one of our methods
+circle1.printCircumference()
+
+## Lets make 2 more circle objects and use the method .printCircumference
+
+circle2 = Circle(5)
+circle2.printCircumference()
+
+circle3 = Circle(7)
+circle3.printCircumference()
